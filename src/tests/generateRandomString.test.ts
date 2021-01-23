e943c0e23f3fe3d8c0be2c4c25a02055592430ce
Ryan Staniforth <rtstaniforth@gmail.com>
2021-01-23 23:49:56 +0000

    Update dependencies.

--- src/tests/generateRandomString.test.ts
+++ src/tests/generateRandomString.test.ts
@@ -1,26 +1,26 @@
 import { InvalidLengthError } from '../errors';
 import { generateSecureRandomString } from '../generateSecureRandomString';
 
-describe('generateSecureRandomString', () => {
-    test('is a promise', async () => {
+describe('generateSecureRandomString', (): void => {
+    test('is a promise', async (): Promise<void> => {
         // tslint:disable-next-line:no-floating-promises
         expect(generateSecureRandomString(1))
             .toBeInstanceOf(Promise);
     });
 
-    test('promise resolves to string', async () => {
+    test('promise resolves to string', async (): Promise<void> => {
         expect(typeof await generateSecureRandomString(1))
             .toBe('string');
     });
 
-    test('string has correct length', async () => {
+    test('string has correct length', async (): Promise<void> => {
         for (const i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
             expect((await generateSecureRandomString(i)).length)
                 .toBe(i);
         }
     });
 
-    test('invalid length argument throws error', async () => {
+    test('invalid length argument throws error', async (): Promise<void> => {
         expect.assertions(2);
 
         try {
